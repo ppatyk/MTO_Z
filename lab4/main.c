755449c26079f73414ceeee2bcc5fbfa96c7b3ce
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-11-06 15:04:04 +0100

    Add files via upload
    przekształcenia przy użyciu zmiennej pomocniczej

--- lab4/main.c
+++ lab4/main.c
@@ -10,3 +10,13 @@ char *strrev(char *str)
     int i = strlen(str) - 1, j = 0;
 
     char ch;
+    while (i > j)
+    {
+        ch = str[i];                
+        str[i] = str[j];           
+        str[j] = ch;                
+        i--;                                                   
+        j++;
+    }
+    return str;
+}
