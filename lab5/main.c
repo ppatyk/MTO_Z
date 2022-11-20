aa0c6cab23d925c3ffb7bef738f95527433ea33a
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-11-20 14:45:38 +0100

    pętla while do decrement string

--- lab5/main.c
+++ lab5/main.c
@@ -14,3 +14,17 @@ char *decrement_string(char *str)
     int i = strlen(str) - 1;  
     int digit;
     char ch;
+
+    while (i >= 0)  // od ostatniego do pierwszego znaku iterujemy po param2
+    {
+    	digit = (int)(str[i]) - '0';   
+    	digit -= 1;                    
+    	if (digit < 0) {               
+    		digit = 9;
+		}
+		ch = digit + '0';             
+		str[i] = ch;                       
+		i--;                  
+    }   
+    return str;
+}
