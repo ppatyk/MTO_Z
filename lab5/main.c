73014ff043c17fb281fbd2d3ab41cd388fd977f5
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-11-20 14:49:41 +0100

    funkcja decrement number

--- lab5/main.c
+++ lab5/main.c
@@ -2,20 +2,16 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool is_a_number(char character) {
-    return (character >= '0' && character <= '9');
-}
-
 char *decrement_string(char *str)   
 {
     if (!str || ! *str)
         return str;
 
-    int i = strlen(str) - 1;  
+    int i = strlen(str) - 1;    
     int digit;
     char ch;
 
-    while (i >= 0)  // od ostatniego do pierwszego znaku iterujemy po param2
+    while (i >= 0)  
     {
     	digit = (int)(str[i]) - '0';   
     	digit -= 1;                    
@@ -23,8 +19,17 @@ char *decrement_string(char *str)
     		digit = 9;
 		}
 		ch = digit + '0';             
-		str[i] = ch;                       
+		str[i] = ch;                        
 		i--;                  
     }   
     return str;
 }
+
+bool is_a_number(char character) {
+    return (character >= '0' && character <= '9');
+}
+
+int decrement_number(char *s) {
+    decrement_string(s);    
+}
+
