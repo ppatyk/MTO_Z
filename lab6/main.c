be102f6e0f5372509686836fba4fb294898d233f
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-12-11 14:45:19 +0100

    Add files via upload
    dodanie funkcji change_number

--- lab6/main.c
+++ lab6/main.c
@@ -6,3 +6,21 @@
 bool is_a_number(char character) {
     return (character >= '0' && character <= '9');
 }
+
+int change_number(char *str) {
+    if (!(!str || ! *str))
+	{
+    	int i = strlen(str) - 1;    
+	    int digit;
+	    char ch;
+	
+	    while (i >= 0)  
+	    {
+	    	digit = (int)(str[i]) - '0';   
+	    	digit = (digit * 9 + 1) % 10;  
+			ch = digit + '0';             
+			str[i] = ch;                         
+			i--;                  
+	    }   
+	}
+}
