22d65598170924e1b91e50a5997554302a398352
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-12-18 15:32:17 +0100

    Add files via upload
    dodanie while

--- lab7/main.c
+++ lab7/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+
 bool is_a_hex_number(char character) {
     return ((character >= '0' && character <= '9') || (character >= 'a' && character <= 'f'));
 }
@@ -21,7 +22,7 @@ int change_number(char *str) {
 			} else {
 				ch = asci_digit;        
 			}
-			str[i] = ch;                        
+			str[i] = ch;                         
 			i--;                  
 	    }   
 	}
@@ -41,7 +42,7 @@ bool is_string_a_number(char *s) {
 int my_printf(char *format_string, char *param, char *result){
 	int i;
 	for(i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'j')){ 
+		if((format_string[i] == '#') && (format_string[i+1] == 'j')){   
 			strcat(result, param);
 			i += 1;
 		}
@@ -53,7 +54,6 @@ int my_printf(char *format_string, char *param, char *result){
 	return 0;
 }
 
-
 int main(int argc, char *argv[]){
 	FILE *fp_input;
 	FILE *fp_output;
@@ -63,3 +63,36 @@ int main(int argc, char *argv[]){
 
 	fp_input = fopen("input.txt", "r");
 	fp_output = fopen("output.txt", "w");
+
+	while(true) {
+		read = getline(&buf, &len, fp_input);
+		read2 = getline(&buf2, &len, fp_input);
+		memset(result, 0, 1024);
+
+		if (read == -1 || read2 == -1) {
+			break;
+		} else {
+			buf[strcspn(buf, "\n")] = 0;
+			buf2[strcspn(buf2, "\n")] = 0;
+			
+			if(is_string_a_number(buf2)) {    
+				printf("\nCorrect Input Format");
+			} else {
+				printf("\nSecond input parameter is not a number!");
+				break;
+			}
+
+			change_number(buf2);    
+			my_printf(buf, buf2, result);    
+
+			fputs(result, fp_output);
+			fputs("\n", fp_output);
+		}
+	}
+
+	fclose(fp_input);
+	fclose(fp_output);
+
+
+	return 0;
+}
