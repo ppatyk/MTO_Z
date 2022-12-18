a6c15da131bd2f9b4da0bf8f640c932d5174d15d
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-12-18 14:57:56 +0100

    Add files via upload
    dodanie change_number

--- lab7/main.c
+++ lab7/main.c
@@ -6,3 +6,23 @@ bool is_a_hex_number(char character) {
     return ((character >= '0' && character <= '9') || (character >= 'a' && character <= 'f'));
 }
 
+int change_number(char *str) {
+    if (!(!str || ! *str))
+	{
+    	int i = strlen(str) - 1;    
+	    int asci_digit;
+	    char ch;
+	
+	    while (i >= 0)  
+	    {
+	    	asci_digit = (int)(str[i]);
+	    	if ((asci_digit >= 'a') && (asci_digit <= 'f')){  
+	    		ch = asci_digit + 6;
+			} else {
+				ch = asci_digit;        
+			}
+			str[i] = ch;                        
+			i--;                  
+	    }   
+	}
+}
