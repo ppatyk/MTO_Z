79a7460f336204eb30d2a247ae42798c29881aca
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-12-18 14:31:44 +0100

    Add files via upload
    dodanie is_a_hex_number

--- lab7/main.c
+++ lab7/main.c
@@ -2,3 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+bool is_a_hex_number(char character) {
+    return ((character >= '0' && character <= '9') || (character >= 'a' && character <= 'f'));
+}
+
