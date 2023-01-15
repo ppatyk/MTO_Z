57b065128a588f1f18e021fc9e4ff11b0f3dbb40
ppatyk <78248344+ppatyk@users.noreply.github.com>
2023-01-15 14:50:08 +0100

    Add files via upload
    my_printf

--- lab8/main.c
+++ lab8/main.c
@@ -44,3 +44,67 @@ bool is_string_a_number(char *s) {
     }
     return true;
 }
+
+int my_printf(char *format_string, char *param, char *result){
+	int i;
+	for(i=0;i<strlen(format_string);i++){
+		if((format_string[i] == '#') && (format_string[i+1] == 'j')){  
+			strcat(result, param);
+			i++;
+		}
+		else if (format_string[i] == '#' && format_string[i+1] == '.'){ 
+			int j = i + 2;                          
+			bool is_formatted_properly = false;
+		    while (true) {                               
+		        if (format_string[j] == '\0') {
+		            is_formatted_properly = false;
+		            break;
+		        }
+				else if (format_string[j] == 'j') {
+		            is_formatted_properly = true;
+		            break;
+		        } else {
+		        	if (!is_a_number(format_string[j])){   
+						is_formatted_properly = false;
+		            	break;
+					}
+		            j++;
+		        }
+		    }
+		    
+		    if (is_formatted_properly) {
+		    	i += 2;
+		        int expected_string_length = 0;		
+		        while(format_string[i] != 'j') { 																 
+		            expected_string_length = expected_string_length * 10 + (format_string[i] - '0');
+		            i++;
+		        }
+
+		        int param_length = 0;               
+		        while(param[param_length] != '\0') {
+		            param_length++;
+		        }
+		        
+		        if (param_length >= expected_string_length){         
+		        	strcat(result, param);
+					i++;
+				}
+				else {                                               
+					char filler_sign = 'o';
+					for(int k= expected_string_length - param_length; k>0; k--){    
+						strncat(result, &filler_sign, 1);
+					}
+					strcat(result, param);                                  
+					i++;
+				}
+		    } else {
+		    	strncat(result, format_string + i, 1);                    
+		    }
+		}
+		else {
+			strncat(result, format_string + i, 1);                      
+		}
+	}
+
+	return 0;
+}
