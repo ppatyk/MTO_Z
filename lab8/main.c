e0173daed0489b35e890221c4b7b9370b2bc73f3
ppatyk <78248344+ppatyk@users.noreply.github.com>
2023-01-15 14:30:48 +0100

    Add files via upload
    dodanie zmiennych

--- lab8/main.c
+++ lab8/main.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+
+bool is_a_hex_number(char character) {
+    return ((character >= '0' && character <= '9') || (character >= 'a' && character <= 'f'));
+}
+
+bool is_a_number(char character) {
+    return (character >= '0' && character <= '9');
+}
