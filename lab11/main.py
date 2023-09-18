a5d318e318e07df216e733b411b91846497c2d67
ppatyk <78248344+ppatyk@users.noreply.github.com>
2023-09-18 14:09:10 +0200

    Add files via upload
    final

--- lab11/main.py
+++ lab11/main.py
@@ -3,25 +3,6 @@
 import sys
 import re
 
-def decimal_to_binary(param):
-    new_param = ''
-    decimal = ''
-    for i in range(len(param)):
-        letter = param[i]
-        if letter.isdigit():
-            decimal += letter
-            if i == len(param) - 1:
-                binary = bin(int(decimal)).replace('0b', '')
-                new_param += binary
-        else:
-            if decimal != '':
-                binary = bin(int(decimal)).replace('0b', '')
-                new_param += binary
-                decimal = ''
-            new_param += letter
-            number = ''
-    return new_param
-
 def replace_letter(number):
     if number == 0:
         return 'a'
@@ -44,20 +25,53 @@ def replace_letter(number):
     elif number == 9:
         return 'j'
 
-def my_printf(format_string, param):
-    new_param = decimal_to_binary(param)
-    format_string = format_string.replace('#b', new_param)
-    result = ""
-    for i in range(len(format_string) - 1, -1, -1):
-        letter = format_string[i]
+def binary_to_letters(binary):
+    result = ''
+    for i in range(len(binary) - 1, -1, -1):
+        letter = binary[i]
         if letter == '1':
-            result = replace_letter((len(format_string) - 1 - i) % 10) + result
+            result = replace_letter((len(binary) - 1 - i) % 10) + result
         else:
             result = letter + result
+    return result
+
+
+def decimal_to_binary(param):
+    new_param = ''
+    decimal = ''
+    for i in range(len(param)):
+        letter = param[i]
+        if letter.isdigit():
+            decimal += letter
+            if i == len(param) - 1:
+                binary = bin(int(decimal)).replace('0b', '')
+                new_param += binary_to_letters(binary)
+        else:
+            if decimal != '':
+                binary = bin(int(decimal)).replace('0b', '')
+                new_param += binary_to_letters(binary)
+                decimal = ''
+            new_param += letter
+            number = ''
+    return new_param
+
+
+
+def my_printf(format_string, param):
+    new_param = decimal_to_binary(param)
+    result = format_string.replace('/#d', new_param)
+    
+
+
     print(result)
 
+    # format_string = format_string.replace('#b', result)
+    # print(format_string)
 
-data = sys.stdin.readlines()
 
-for i in range(0, len(data), 2):
-    my_printf(data[i].rstrip(), data[i+1].rstrip())
+my_printf('TEST1 #b TEST1', '5a5a5')
+
+# data = sys.stdin.readlines()
+
+# for i in range(0, len(data), 2):
+#     my_printf(data[i].rstrip(), data[i+1].rstrip())
