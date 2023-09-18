600c126201d74131aca4e7158413c8725f4a3b25
ppatyk <78248344+ppatyk@users.noreply.github.com>
2023-09-18 13:54:31 +0200

    Add files via upload
    replace letter

--- lab11/main.py
+++ lab11/main.py
@@ -6,26 +6,58 @@ import re
 def decimal_to_binary(param):
     new_param = ''
     decimal = ''
-    for letter in param:
+    for i in range(len(param)):
+        letter = param[i]
         if letter.isdigit():
             decimal += letter
+            if i == len(param) - 1:
+                binary = bin(int(decimal)).replace('0b', '')
+                new_param += binary
         else:
             if decimal != '':
                 binary = bin(int(decimal)).replace('0b', '')
                 new_param += binary
+                decimal = ''
             new_param += letter
             number = ''
     return new_param
 
-
+def replace_letter(number):
+    if number == 0:
+        return 'a'
+    elif number == 1:
+        return 'b'
+    elif number == 2:
+        return 'c'
+    elif number == 3:
+        return 'd'
+    elif number == 4:
+        return 'e'
+    elif number == 5:
+        return 'f'
+    elif number == 6:
+        return 'g'
+    elif number == 7:
+        return 'h'
+    elif number == 8:
+        return 'i'
+    elif number == 9:
+        return 'j'
 
 def my_printf(format_string, param):
-    result = ""
     new_param = decimal_to_binary(param)
-
-print(decimal_to_binary('5a5a5'))
+    format_string = format_string.replace('#b', new_param)
+    result = ""
+    for i in range(len(format_string) - 1, -1, -1):
+        letter = format_string[i]
+        if letter == '1':
+            result = replace_letter((len(format_string) - 1 - i) % 10) + result
+        else:
+            result = letter + result
+    print(result)
 
 
 data = sys.stdin.readlines()
+
 for i in range(0, len(data), 2):
-my_printf(data[i].rstrip(), data[i+1].rstrip())
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
